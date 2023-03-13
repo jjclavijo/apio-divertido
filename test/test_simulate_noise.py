d01355a882d96251c625e0c56d52507016be4f95
Javier Clavijo <jjclavijo@gmail.com>
2023-03-13 15:09:20 -0300

    Tratando de sacar todo lo que sea compat de todos lados.
    En el test de simulate_noise uso la funcion vector_to_mom de compat para
    convertir el ruido que está en un array a una string tipo ".mom" que
    después hasheo.

--- test/test_simulate_noise.py
+++ test/test_simulate_noise.py
@@ -9,6 +9,7 @@ from pathlib import Path
 from hashlib import md5
 
 from eletor.simulatenoise import simulate_noise
+from eletor.compat import vector_to_mom
 
 #
 # Test de simulate_noise por casos
@@ -27,42 +28,33 @@ def test_simulate_noise_cases(hector_test_cases,modelo,etiqueta,parametros):
     # Agregar las opciones del modelo a testear
     data['NoiseModels'][modelo][etiqueta] = parametros
 
-    # Usamos un archivo temporal, asi se borra solo.
-    # El sufijo es importante porque por compatibilidad con
-    # hector siempre se agregaba ese sufijo.
-    with tempfile.NamedTemporaryFile(suffix='_0.mom') as f:
-        # Configuracion de archivo de salida para hector
-        fname = Path(f.name)
-        data['file_config']['SimulationLabel'] = fname.name.replace('_0.mom','')
-        data['file_config']['SimulationDir'] = str(fname.parent)
+    # Correr la simulación (devuelve lista, pero es sólo 1)
+    sim,*_ = simulate_noise(data)
 
-        # Correr la simulación
-        simulate_noise(data)
+    # Generar el Hash
+    md5hash = md5()
+    datalines = vector_to_mom(y=sim,sampling_period=parametros['dt'])
+    for line in datalines:
+        md5hash.update(line.encode())
 
-        # Generar el Hash
-        md5hash = md5()
-        with fname.open(mode='rb') as f:
-            bdata = f.read()
-            md5hash.update(bdata)
+    # En caso de que hubiera algún test que fallara y hubiera que
+    # revisar el mom directamente, había que hacer algo como:
+    # if md5hash.hexdigest() != parametros['md5']:
+    #   # Grabar la salida nuestra
+    #   with open('failed_test_{modelo}_{etiqueta}.mom','wb') as f:
+    #     f.write(bdata)
+    #
+    #   # Llamar al hector original
+    #   from call_old_hector import call_hector_with_toml
+    #   call_hector_with_toml(data,return_type='file')
+    #
+    #   # Copiar la salida de hector.
+    #   with fname.open(mode='rb') as f:
+    #     with open('hector_{modelo}_{etiqueta}.mom','wb') as f:
+    #       f.write(bdata)
+    # Esto queda comentado por la premisa de que nada que
+    # tenga que ver con hector tiene que quedar por default activo.
 
-        # En caso de que hubiera algún test que fallara y hubiera que
-        # revisar el mom directamente, había que hacer algo como:
-        # if md5hash.hexdigest() != parametros['md5']:
-        #   # Grabar la salida nuestra
-        #   with open('failed_test_{modelo}_{etiqueta}.mom','wb') as f:
-        #     f.write(bdata)
-        #
-        #   # Llamar al hector original
-        #   from call_old_hector import call_hector_with_toml
-        #   call_hector_with_toml(data,return_type='file')
-        #
-        #   # Copiar la salida de hector.
-        #   with fname.open(mode='rb') as f:
-        #     with open('hector_{modelo}_{etiqueta}.mom','wb') as f:
-        #       f.write(bdata)
-        # Esto queda comentado por la premisa de que nada que
-        # tenga que ver con hector tiene que quedar por default activo.
-
-        assert md5hash.hexdigest() == parametros['md5']
+    assert md5hash.hexdigest() == parametros['md5']
 
 
